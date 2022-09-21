30737a8faa585a24d0b6aaa8cfcb232e92f3ab77
Manish <manishregmi39@gmail.com>
2022-09-21 12:40:23 +0545

    first commit

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    
+    function login()
+    {
+        return view('login');
+    }
+
+    function checkCredentials(Request $request)
+    {
+        $user = ['admin@gmail.com','password'];
+        if($user[0] == $request->email)
+        {
+            if($user[1] == $request->password)
+            {
+                return redirect('/movies-index');
+            }else{
+                return back();
+            }
+        }else{
+            return back();
+        }
+    }
+    function movies()
+    {
+        return view('movies.index');
+    }
+}
---
+++ resources/views/login.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+</head>
+
+<body class="bg-primary">
+    <div class="container">
+        <h1 class="text-center text-white m-4">Login to continue</h1>
+        <div class="card p-5">
+            <form action="/check-creadentials" method="post">
+                @csrf
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" name="password" id="exampleInputPassword1">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+</body>
+
+</html>
